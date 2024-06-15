f6905a97a6fea43c3a4f4f33a5742130a021fe2e
Alif T <alift929@gmail.com>
2024-06-16 06:15:17 +0700

    refactor package

--- assignment/lecture2/Assignment7/src/main/java/org/example/Main.java
+++ assignment/lecture2/Assignment7/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.app.AppManager;
+
 public class Main {
     public static void main(String[] args) {
         AppManager appManager = AppManager.getInstance();
