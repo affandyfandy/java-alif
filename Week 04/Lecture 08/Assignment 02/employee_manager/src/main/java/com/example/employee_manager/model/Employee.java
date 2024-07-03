523325cbbb9db06d02795ba3e52b402622595759
Alif T <alift929@gmail.com>
2024-07-03 13:52:23 +0700

    Feat: add assignment 2

---
+++ Week 04/Lecture 08/Assignment 02/employee_manager/src/main/java/com/example/employee_manager/model/Employee.java
@@ -0,0 +1,14 @@
+package com.example.employee_manager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Employee {
+    private String id;
+    private String name;
+    private String address;
+    private String department;
+}
