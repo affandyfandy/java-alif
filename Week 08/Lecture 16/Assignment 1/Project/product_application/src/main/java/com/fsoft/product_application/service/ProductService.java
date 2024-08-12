cbb4a0cc5f382b4fc5492d4b5d9e000ecfaf17cb
Alif T <alift929@gmail.com>
2024-08-12 11:28:33 +0700

    feat: add invoice and product application
    Forgot to add this

---
+++ Week 08/Lecture 16/Assignment 1/Project/product_application/src/main/java/com/fsoft/product_application/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.fsoft.product_application.service;
+
+import com.fsoft.product_application.model.Product;
+import com.fsoft.product_application.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+}
