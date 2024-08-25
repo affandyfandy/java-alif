78cc5d644bd83eafa834df6a6e6aaa018cc35e41
Alif T <alift929@gmail.com>
2024-08-25 14:22:43 +0700

    refactor: change Angular version 18  to 17

--- Week 11/Assignment/assignment-app/src/app/app.routes.ts
+++ Week 11/Assignment/assignment-app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'home', component: HomeComponent}
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent},
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
